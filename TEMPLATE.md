36463945e8c38973416cdb103186251cbda43e83
Lukas Geiger <lukas.geiger94@gmail.com>
2016-11-30 13:29:25 +0100

    Initial commit

---
+++ TEMPLATE.md
@@ -0,0 +1,6 @@
+# Electron ABI
+
+The following ABIs are extracted by running `./list-abi.sh`.
+
+Electron | ABI
+---------|------
---
+++ list-abi.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ELECTRON=./node_modules/electron-prebuilt/cli.js
+cp TEMPLATE.md README.md
+
+cat versions.txt | while read version; do
+  npm install electron-prebuilt@$version
+  (echo -n "$version | "; ELECTRON_RUN_AS_NODE=1 $ELECTRON abi.js) >> README.md
+  rm -rf node_modules/electron-prebuilt
+done
