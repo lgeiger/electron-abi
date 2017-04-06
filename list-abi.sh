39eb4df1656963d8ae266512ee646525b5e864e3
milligramme <milligramme.cc@gmail.com>
2017-04-07 00:29:40 +0900

    Install electron instead of electron-prebuilt from v1.3.1

--- list-abi.sh
+++ list-abi.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-ELECTRON=./node_modules/electron-prebuilt/cli.js
 cp TEMPLATE.md README.md
 
 cat versions.txt | while read version; do
-  npm install electron-prebuilt@$version
+  
+  if [[ $version > 1.3 ]] && [[ $version != 1.3.0 ]]; then
+    prebuilt=""
+  else
+    prebuilt="-prebuilt"
+  fi
+  npm install electron$prebuilt@$version
+  ELECTRON=./node_modules/electron$prebuilt/cli.js
   (echo -n "$version | "; ELECTRON_RUN_AS_NODE=1 $ELECTRON abi.js) >> README.md
-  rm -rf node_modules/electron-prebuilt
+  rm -rf node_modules/electron$prebuilt
+
 done
 cat README.md
